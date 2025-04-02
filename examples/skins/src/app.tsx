8243a786e73d31760621f373e78d467d6e7f0283
Richie <ricsam@users.noreply.github.com>
2025-04-03 01:57:07 +0200

    update examples

--- examples/skins/src/app.tsx
+++ examples/skins/src/app.tsx
@@ -7,14 +7,14 @@ import { DefaultThemeProvider } from "./default_theme_provider";
 import { useBigTable } from "./use_big_table";
 import { useHashState } from "./use_hash_state";
 import { Table } from "@tanstack/react-table";
+import { TailwindSkin, darkModeVars, lightModeVars } from "@rttui/skin-tailwind";
 
 export function App() {
   const { table } = useBigTable();
   const [theme, setTheme] = useHashState<"light" | "dark">("theme", "light");
-  const [skin, setSkin] = useHashState<"mui" | "anocca" | "default">(
-    "skin",
-    "mui",
-  );
+  const [skin, setSkin] = useHashState<
+    "mui" | "anocca" | "default" | "tailwind"
+  >("skin", "mui");
 
   const activeSkin = React.useMemo(() => {
     switch (skin) {
@@ -22,6 +22,8 @@ export function App() {
         return MuiSkin;
       case "anocca":
         return AnoccaSkin;
+      case "tailwind":
+        return TailwindSkin;
       default:
         return defaultSkin;
     }
@@ -37,12 +39,21 @@ export function App() {
     [theme],
   );
 
+  let twStyle: Record<string, string> = {};
+  if (skin === "tailwind") {
+    if (theme === "light") {
+      twStyle = lightModeVars;
+    } else {
+      twStyle = darkModeVars;
+    }
+  }
+
   return (
     <DefaultThemeProvider theme={theme}>
       <ThemeProvider theme={muiTheme}>
         <CssBaseline />
         <div style={{ textAlign: "center" }}>
-          <div style={{ marginBottom: "20px" }}>
+          <div style={{ marginBottom: "20px", ...twStyle }}>
             <select
               value={theme}
               onChange={(e) => setTheme(e.target.value as "light" | "dark")}
@@ -55,11 +66,12 @@ export function App() {
             <select
               value={skin}
               onChange={(e) =>
-                setSkin(e.target.value as "mui" | "anocca" | "default")
+                setSkin(e.target.value as "mui" | "anocca" | "default" | "tailwind")
               }
             >
               <option value="mui">Material UI Skin</option>
               <option value="anocca">Anocca Skin</option>
+              <option value="tailwind">Tailwind Skin</option>
               <option value="default">Default Skin</option>
             </select>
           </div>
