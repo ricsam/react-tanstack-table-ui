d595a0cece2d8ca768cbf37f909f1d0ebda7df59
Richie <ricsam@users.noreply.github.com>
2025-04-02 10:24:30 +0200

    remove happy-dom

--- test/lib-import/vite.config.ts
+++ test/lib-import/vite.config.ts
@@ -6,7 +6,6 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   test: {
-    environment: "happy-dom",
     browser: {
       enabled: true,
       provider: "playwright",
