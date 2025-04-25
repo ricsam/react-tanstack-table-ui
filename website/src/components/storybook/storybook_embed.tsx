adfaffd7f5d4b35702ee4a285418d86d522d4902
Richie <ricsam@users.noreply.github.com>
2025-04-25 08:06:16 +0200

    storybook updates

--- website/src/components/storybook/storybook_embed.tsx
+++ website/src/components/storybook/storybook_embed.tsx
@@ -1,10 +1,13 @@
 interface StorybookEmbedProps {
   skin: "mui" | "tailwind" | "bleu" | "core";
+  storyId?: string;
 }
 
-export function StorybookEmbed({ skin }: StorybookEmbedProps) {
-  // const storybookUrl = `https://rttui-docs.vercel.app/static_sb/skins/index.html?path=/docs/reacttanstacktableui--docs#sb_skin=${skin}`;
-  const storybookUrl = `http://localhost:6006/?path=/docs/reacttanstacktableui--docs#sb_skin=${skin}`;
+export function StorybookEmbed({ skin, storyId }: StorybookEmbedProps) {
+  let storybookUrl = `https://rttui-docs.vercel.app/static_sb/skins/index.html?path=/docs/reacttanstacktableui--docs#sb_skin=${skin}`;
+  if (storyId) {
+    storybookUrl += `https://rttui-docs.vercel.app/static_sb/skins/index.html?globals=&args=skin%3A${skin}&id=${storyId}&viewMode=story#sb_skin=${skin}`;
+  }
 
   return (
     <div className="w-full h-[calc(100vh-64px)] bg-gray-50 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg overflow-hidden">
---
+++ website/src/routes/skins/tailwind/style.tsx
@@ -0,0 +1,18 @@
+import { StorybookEmbed } from "@/components/storybook/storybook_embed";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/skins/tailwind/style")({
+  component: RouteComponent,
+  staticData: {
+    layout: "full",
+  },
+});
+
+function RouteComponent() {
+  return (
+    <StorybookEmbed
+      skin="tailwind"
+      storyId="reacttanstacktableui--full-example"
+    />
+  );
+}
